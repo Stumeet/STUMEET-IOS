a94fbd495e713164a8215507c632ae9c1421f855
Hohyeon Moon <hohyeonmoon@gmail.com>
2024-02-06 00:08:47 +0900

    Kakao login initial settings

--- Stumeet/Stumeet/StumeetApp.swift
+++ Stumeet/Stumeet/StumeetApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct StumeetApp: App {
+    init() {
+        KakaoSDK.initSDK(appKey: "NATIVE_APP_KEY")
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onOpenURL { url in
+                    if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                        _ = AuthController.handleOpenUrl(url: url)
+                    }
+                }
         }
     }
 }
