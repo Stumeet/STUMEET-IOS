07bb590deeb65666e214e89dff21389d74c7bd6f
Hohyeon Moon <hohyeonmoon@gmail.com>
2024-02-05 21:17:27 +0900

    Init project

---
+++ Stumeet/Stumeet/StumeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  StumeetApp.swift
+//  Stumeet
+//
+//  Created by Hohyeon Moon on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct StumeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
