83bdfdb7016c02aa6f6e87dd605267ee19805cbc
jihun32 <jihun32@naver.com>
2024-02-10 14:26:05 +0900

    [STMT-21] Feat: Extension 함수 구현

--- Stumeet/Stumeet/Utilities/Extension/UILabel++Extension.swift
+++ Stumeet/Stumeet/Utilities/Extension/UILabel++Extension.swift
@@ -5,4 +5,26 @@
 //  Created by 정지훈 on 2/10/24.
 //
 
-import Foundation
+import UIKit
+
+extension UILabel {
+    
+    
+    /// LabelProperty를 설정하는 함수입니다.
+    /// - Parameters:
+    ///   - text: String
+    ///   - font: UIFont
+    ///   - color: StumeetColor
+    func setLabelProperty(text: String?, font: UIFont, color: StumeetColor?) -> UILabel {
+        if let text = text {
+            self.text = text
+        }
+        self.font = font
+        
+        if let color = color?.color {
+            self.textColor = color
+        }
+        
+        return self
+    }
+}
