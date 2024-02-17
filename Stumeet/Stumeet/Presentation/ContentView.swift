85a8b60532a3ffa0860d7d2f35bbd89ff917cf50
jihun32 <jihun32@naver.com>
2024-02-17 18:14:06 +0900

    [STMT-21] Refactor: contentView 원래대로 돌리기

--- Stumeet/Stumeet/Presentation/ContentView.swift
+++ Stumeet/Stumeet/Presentation/ContentView.swift
@@ -7,33 +7,17 @@
 
 import SwiftUI
 
-struct FirstViewControllerWrapper: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> UINavigationController {
-        let navigationContorller = UINavigationController()
-        let registerCoordinator = RegisterCoordinator(navigationController: navigationContorller)
-        registerCoordinator.start()
-        
-        return navigationContorller
-    }
-    
-    func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {}
-}
-
 struct ContentView: View {
-    @State private var isShowingProfileVC = false
-
     var body: some View {
         VStack {
-            Button("프로필 변경하기") {
-                self.isShowingProfileVC = true
-            }
-            .fullScreenCover(isPresented: $isShowingProfileVC) {
-                FirstViewControllerWrapper()
-            }
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
-
 
 #Preview {
     ContentView()
