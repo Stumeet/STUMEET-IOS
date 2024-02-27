e4a41c8bdc339834273f7926ea7bbae1e05e9073
jihun32 <jihun32@naver.com>
2024-02-27 16:58:08 +0900

    [STMT-26] Refactor: model entity로 이동

--- Stumeet/Stumeet/Domain/Entity/Register/Region.swift
+++ Stumeet/Stumeet/Domain/Entity/Register/Region.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct Region: Hashable {
+    let region: String
+    var isSelected: Bool
+}
+
+extension Region {
+    static var list: [Region] = {[
+        Region(region: "서울", isSelected: false),
+        Region(region: "인천/경기", isSelected: false),
+        Region(region: "전북", isSelected: false),
+        Region(region: "전남", isSelected: false),
+        Region(region: "강원", isSelected: false),
+        Region(region: "경북", isSelected: false),
+        Region(region: "경남", isSelected: false),
+        Region(region: "충북", isSelected: false),
+        Region(region: "충남", isSelected: false),
+        Region(region: "제주", isSelected: false)
+    ]}()
+}
