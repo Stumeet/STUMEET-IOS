7a4aec7eaf485851e296e954d51e8d0d358e37b3
jihun32 <jihun32@naver.com>
2024-03-02 16:21:58 +0900

    [STMT-124] Feat: Encodable extension 추가

--- Stumeet/Stumeet/Utilities/Extension/Encodable++Extension.swift
+++ Stumeet/Stumeet/Utilities/Extension/Encodable++Extension.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+import Foundation
+
+extension Encodable {
+    
+    /// Request parameter를 encoding해 dictionary로 바꿔줍니다.
+    var toDictionary : [String: Any]? {
+        guard let object = try? JSONEncoder().encode(self) else { return nil }
+        guard let dictionary = try? JSONSerialization.jsonObject(with: object, options: []) as? [String:Any] else { return nil }
+        return dictionary
+    }
+}
--- Stumeet/Stumeet/Utilities/Extension/EncodableExtension++.swift
+++
@@ -1,8 +0,0 @@
-//
-//  EncodableExtension++.swift
-//  Stumeet
-//
-//  Created by 정지훈 on 3/2/24.
-//
-
-import Foundation
