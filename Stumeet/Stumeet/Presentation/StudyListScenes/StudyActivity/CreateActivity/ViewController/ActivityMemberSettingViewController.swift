5afaba0b114d9a02eee5cac5293edafa74a10303
jihun32 <jihun32@naver.com>
2024-05-13 16:43:38 +0900

    [STMT-206] UI: 스터디 활동 멤버 설정 UI Component 배치

--- Stumeet/Stumeet/Presentation/StudyListScenes/StudyActivity/CreateActivity/ViewController/ActivityMemberSettingViewController.swift
+++ Stumeet/Stumeet/Presentation/StudyListScenes/StudyActivity/CreateActivity/ViewController/ActivityMemberSettingViewController.swift
@@ -5,10 +5,68 @@
 //  Created by 정지훈 on 5/13/24.
 //
 
-import Foundation
+import UIKit
 
 class ActivityMemberSettingViewController: BaseViewController {
     // MARK: - UIComponents
+    
+    private let xButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "xMark"), for: .normal)
+        
+        return button
+    }()
+    
+    private let titleLabel: UILabel = {
+        return UILabel().setLabelProperty(text: "참여 멤버", font: StumeetFont.titleMedium.font, color: nil)
+    }()
+    
+    private let searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "멤버 이름 검색"
+        textField.backgroundColor = StumeetColor.primary50.color
+        textField.layer.cornerRadius = 10
+        
+        let rightImageView = UIImageView(image: UIImage(named: "search"))
+        
+        let rightViewContainer = UIView()
+        rightViewContainer.frame = CGRect(x: 0, y: 0, width: rightImageView.frame.width, height: rightImageView.frame.height)
+        rightImageView.frame = CGRect(x: -24, y: 0, width: rightImageView.frame.width, height: rightImageView.frame.height)
+        rightViewContainer.addSubview(rightImageView)
+        
+        textField.rightView = rightViewContainer
+        textField.rightViewMode = .always
+        textField.addLeftPadding(24)
+        
+        return textField
+    }()
+    
+    private let allSelectButton: UIButton = {
+        let button = UIButton()
+        var config = UIButton.Configuration.plain()
+        
+        var titleAttr = AttributedString.init("전체 선택")
+        titleAttr.font = StumeetFont.bodyMedium14.font
+        titleAttr.foregroundColor = StumeetColor.gray400.color
+        config.attributedTitle = titleAttr
+        
+        config.image = UIImage(named: "memberUnSelectedButton")
+        config.imagePlacement = .leading
+        config.imagePadding = 8
+        button.configuration = config
+        
+        return button
+    }()
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        
+        return tableView
+    }()
+    
+    private let completeButton: UIButton = {
+        return UIButton().makeRegisterBottomButton(text: "완료", color: StumeetColor.gray200.color)
+    }()
     
     // MARK: - Properties
     
@@ -38,11 +96,51 @@ class ActivityMemberSettingViewController: BaseViewController {
     }
     
     override func setupAddView() {
-        
+        [
+            xButton,
+            titleLabel,
+            searchTextField,
+            allSelectButton,
+            tableView,
+            completeButton
+        ]   .forEach { view.addSubview($0) }
     }
     
     override func setupConstaints() {
         
+        xButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(16)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(12)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.centerY.equalTo(xButton)
+        }
+        
+        searchTextField.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.top.equalTo(titleLabel.snp.bottom).offset(28)
+            make.height.equalTo(56)
+        }
+        
+        allSelectButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(16)
+            make.top.equalTo(searchTextField.snp.bottom).offset(24)
+            make.height.equalTo(84)
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(allSelectButton.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(432)
+        }
+        
+        completeButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(34)
+            make.height.equalTo(72)
+        }
     }
     
     // MARK: - Bind
