58e6f202367a41ff6f0cce5f39e333e6a595fa45
cwh2626 <52994666+cwh2626@users.noreply.github.com>
2024-06-13 01:09:44 +0900

    [STMT-21] ♻️ 로그인 서비스 의존성 주입 형식으로 변경

--- Stumeet/Stumeet/Presentation/AuthScenes/SnsLogin/ViewModel/SnsLoginViewModel.swift
+++ Stumeet/Stumeet/Presentation/AuthScenes/SnsLogin/ViewModel/SnsLoginViewModel.swift
@@ -19,21 +19,17 @@ final class SnsLoginViewModel: ViewModelType {
     
     // MARK: - Output
     struct Output {
-        let navigateToChangeProfileVC: AnyPublisher<Void, Never>
+        let navigateToChangeProfileVC: AnyPublisher<Bool, Never>
         let showError: AnyPublisher<String, Never>
     }
     
     // MARK: - Properties
     private let showErrorSubject = PassthroughSubject<String, Never>()
-    private var useCase: AuthUseCase!
-    private var repository: LoginRepository
-    private var keychainManager: KeychainManageable
+    private var useCase: AuthUseCase
     
     // MARK: - Init
-    init(repository: LoginRepository,
-         keychainManager: KeychainManageable) {
-        self.repository = repository
-        self.keychainManager = keychainManager
+    init(useCase: AuthUseCase) {
+        self.useCase = useCase
     }
     
     // MARK: - Transform
@@ -42,8 +38,6 @@ final class SnsLoginViewModel: ViewModelType {
             .flatMap { [weak self] type -> AnyPublisher<Bool, Error> in
                 guard let self = self
                 else { return Empty().eraseToAnyPublisher() }
-                let service: LoginService = self.service(for: type)
-                self.useCase = DefaultLoginUseCase(service: service, repository: self.repository, keychainManager: self.keychainManager)
                 
                 return useCase.signIn(loginType: type)}
             .catch { [weak self] error -> AnyPublisher<Bool, Never> in
@@ -54,44 +48,12 @@ final class SnsLoginViewModel: ViewModelType {
                 self.showErrorSubject.send(error.localizedDescription)
                 
                 return Just(false).eraseToAnyPublisher()
-            }
-            .filter { $0 == true }
-            .map { _ in }
+            }.map { $0 }
             .eraseToAnyPublisher()
         
         return Output(
             navigateToChangeProfileVC: navigateToChangeProfileVC,
             showError: showErrorSubject.eraseToAnyPublisher()
         )
-    }
-
-    private func service(for type: LoginType) -> LoginService {
-        switch type {
-        case .apple: return AppleLoginService()
-        case .kakao: return KakaoLoginService()
-        }
-    }
-}
-
-enum LoginType {
-    case apple
-    case kakao
-    
-    var korean: String {
-        switch self {
-        case .apple:
-            return "애플"
-        case .kakao:
-            return "카카오"
-        }
-    }
-    
-    var english: String {
-        switch self {
-        case .apple:
-            return "apple"
-        case .kakao:
-            return "kakao"
-        }
     }
 }
