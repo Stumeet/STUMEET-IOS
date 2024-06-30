b3ed95c03078d41e7e7e78438640eadd9ab00f65
cwh2626 <52994666+cwh2626@users.noreply.github.com>
2024-06-30 13:16:35 +0900

    [STMT-282] ✨앱 첫 실행 시 키체인 정보 초기화 기능 추가

--- Stumeet/Stumeet/Application/AppCoordinator.swift
+++ Stumeet/Stumeet/Application/AppCoordinator.swift
@@ -13,9 +13,19 @@ final class AppCoordinator: Coordinator {
     var navigationController: UINavigationController
     private let appDIContainer: AppDIContainer
     
+    private func removeKeychainAtFirstLaunch() {
+        guard UserDefaults.isFirstLaunch() else {
+            return
+        }
+        appDIContainer.keychainManager.removeAllTokens()
+    }
+    
     func start() {
         // !IMP: - 로그아웃 구현 시 삭제 (임시 로그아웃 처리 필요에 따라 주석 해제 후 사용)
         // appDIContainer.keychainManager.removeAllTokens()
+        
+        removeKeychainAtFirstLaunch()
+        
         let isLoggedIn = appDIContainer.keychainManager.getToken() != nil
         
         if isLoggedIn {
