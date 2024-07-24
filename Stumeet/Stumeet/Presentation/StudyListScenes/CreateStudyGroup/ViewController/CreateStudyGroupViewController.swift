9e7179a7498819a0ef1036ca7dd7f991364cb3e0
정지훈 <jihun332@gmail.com>
2024-07-24 16:26:41 +0900

    [STMT-42] ✨스터디 그룹 생성 코디네이터, 화면 전환 구현

---
+++ Stumeet/Stumeet/Presentation/StudyListScenes/CreateStudyGroup/ViewController/CreateStudyGroupViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CreateStudyGroupViewController.swift
+//  Stumeet
+//
+//  Created by 정지훈 on 7/24/24.
+//
+
+import UIKit
+
+class CreateStudyGroupViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func setupStyles() {
+        view.backgroundColor = .white
+    }
+}
