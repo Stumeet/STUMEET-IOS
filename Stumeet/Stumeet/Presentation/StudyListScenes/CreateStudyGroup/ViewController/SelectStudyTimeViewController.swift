d659c0f8ff2c52f86ca914b3831808c58310cc9c
정지훈 <jihun332@gmail.com>
2024-08-05 21:55:36 +0900

    [STMT-44] 💄스터디 시간 선택 뷰 UI 구현

--- Stumeet/Stumeet/Presentation/StudyListScenes/CreateStudyGroup/ViewController/SelectStudyTimeViewController.swift
+++ Stumeet/Stumeet/Presentation/StudyListScenes/CreateStudyGroup/ViewController/SelectStudyTimeViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-import UIKit
-
 final class SelectStudyTimeViewController: BaseViewController {
     
     // MARK: - UIComponents
     
+    private let backgroundButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.alpha = 0
+        
+        return button
+    }()
+    
+    private let bottomSheetView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 32
+        view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        view.clipsToBounds = true
+        
+        return view
+    }()
+    
+    private let dragIndicatorContainerView = UIView()
+    
+    private let dragIndicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 0.8797428012, green: 0.8797428012, blue: 0.8797428012, alpha: 1)
+        view.layer.cornerRadius = 7
+        return view
+        
+    }()
+    
     private let timeView = TimeView()
+    
+    private let completeButton = UIButton().makeRegisterBottomButton(text: "완료", color: StumeetColor.gray200.color)
     
     // MARK: - Properties
     
@@ -40,6 +68,11 @@ final class SelectStudyTimeViewController: BaseViewController {
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showBottomSheet()
+    }
+    
     // MARK: - SetUp
     
     override func setupStyles() {
@@ -48,10 +81,58 @@ final class SelectStudyTimeViewController: BaseViewController {
     
     override func setupAddView() {
         
+        [
+            dragIndicatorView
+        ]   .forEach(dragIndicatorContainerView.addSubview)
+        
+        [
+            dragIndicatorContainerView,
+            timeView,
+            completeButton
+        ]   .forEach(bottomSheetView.addSubview)
+        
+        [
+            backgroundButton,
+            bottomSheetView
+        ]   .forEach(view.addSubview)
     }
     
     override func setupConstaints() {
         
+        backgroundButton.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        bottomSheetView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(0)
+            make.bottom.equalToSuperview()
+        }
+        
+        dragIndicatorContainerView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(24)
+            make.height.equalTo(30)
+            make.width.equalTo(72)
+        }
+        
+        dragIndicatorView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalToSuperview()
+            make.height.equalTo(8)
+        }
+        
+        timeView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(dragIndicatorView.snp.bottom).offset(24)
+            make.height.equalTo(267)
+        }
+        
+        completeButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.top.equalTo(dragIndicatorView.snp.bottom).offset(318)
+            make.height.equalTo(72)
+        }
     }
     
     override func bind() {
@@ -59,3 +140,17 @@ final class SelectStudyTimeViewController: BaseViewController {
     }
 }
 
+
+// MARK: - UIUpdate
+
+extension SelectStudyTimeViewController {
+    private func showBottomSheet() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.bottomSheetView.snp.updateConstraints { make in
+                make.height.equalTo(456)
+            }
+            self.backgroundButton.alpha = 0.1
+            self.view.layoutIfNeeded()
+        })
+    }
+}
